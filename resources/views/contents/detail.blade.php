d847c89e00437d4237218c2672be58380a715525
大川峻 <okawashun@MacBook-Pro.local>
2020-10-15 15:17:52 +0900

    Laravel8_CRUD処理を使った投稿アプリを作成する_その6_投稿編集機能の作成
    実施漏れのコードを追加

--- resources/views/contents/detail.blade.php
+++ resources/views/contents/detail.blade.php
@@ -3,3 +3,5 @@
 <p>投稿ID: {{$item['id']}}</p>
 <p>投稿内容: {{$item['content']}}</p>
 <p>投稿時間: {{$item['created_at']}}</p>
+{{-- 下記を追記する --}}
+<a href="{{route('edit', ['content_id' => $item['id']])}}">編集</a>
