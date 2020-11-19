96a25c1d12576d8c45bcbb179f11ec7b10cf1a42
大川峻 <okawashun@MacBook-Pro.local>
2020-11-19 09:28:04 +0900

    Laravel8_CRUD処理を使った投稿アプリを作成する_その10_URLをリンク化する
    作業完了

--- resources/views/contents/detail.blade.php
+++ resources/views/contents/detail.blade.php
@@ -4,13 +4,12 @@
 
 <h1>detail</h1>
 
-{{-- 下記を追記する --}}
 @if (isset($item['file_path']))
 <img src="{{asset('storage/' . $item['file_path'])}}" alt="{{asset('storage/' . $item['file_path'])}}">
 @endif
-{{-- 上記までを追記する --}}
 <p>投稿ID: {{$item['id']}}</p>
-<p>投稿内容: {{$item['content']}}</p>
+{{-- 下記を修正する --}}
+<p>投稿内容: {!! nl2br(preg_replace('/(https?:\/\/[^\s]*)/', '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>', $item['content'])) !!}</p>
 <p>投稿時間: {{$item['created_at']}}</p>
 <a href="{{route('edit', ['content_id' => $item['id']])}}">編集</a>
 <form action="{{route('delete')}}" method="post">
--- resources/views/contents/output.blade.php
+++ resources/views/contents/output.blade.php
@@ -6,12 +6,11 @@
 
 @foreach ($items as $item)
 <hr>
-{{-- 下記を追記する --}}
 @if (isset($item['file_path']))
 <img src="{{asset('storage/' . $item['file_path'])}}" alt="{{asset('storage/' . $item['file_path'])}}">
 @endif
-{{-- 上記までを追記する --}}
-<p>{{$item['content']}}</p>
+{{-- 下記を修正する --}}
+<p>{!! nl2br(preg_replace('/(https?:\/\/[^\s]*)/', '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>', $item['content'])) !!}</p>
 <a href="{{route('detail', ['content_id' => $item['id']])}}">詳細</a>
 <a href="{{route('edit', ['content_id' => $item['id']])}}">編集</a>
 @endforeach
